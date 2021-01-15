b27739c241d301c15aa1dfd0b3b58393ef30795a
DawidKrzoska <dawidwilk19999@gmai.com>
2021-01-15 14:01:05 +0100

    first small project ready

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -1,8 +1,30 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const [buttonName, setButtonName] = useState("Clear All");
+
+  const clickHandle = () => {
+    if (Array.isArray(people) && people.length) {
+      setPeople([]);
+
+      setButtonName("Revert All");
+    } else {
+      setPeople(data);
+      setButtonName("Clear All");
+    }
+  };
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthday today</h3>
+        <List people={people} />
+        <button onClick={() => clickHandle()}>{buttonName}</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
