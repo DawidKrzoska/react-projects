ca77781fdbb3403281d7500516c28ce10a9bd304
DawidKrzoska <dawidwilk19999@gmai.com>
2021-02-21 17:51:48 +0100

    sidebar ready

--- 12-sidebar-modal/setup/src/Sidebar.js
+++ 12-sidebar-modal/setup/src/Sidebar.js
@@ -1,10 +1,43 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
-
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+import { useGlobalContext } from "./context";
 const Sidebar = () => {
-  return <h2>sidebar</h2>
-}
+  const { isSideBarOpen, toogleSideBar } = useGlobalContext();
+  return (
+    <aside className={`${isSideBarOpen ? "sidebar show-sidebar" : "sidebar"} `}>
+      <div className="sidebar-header">
+        <img src={logo} className="logo" alt="codingaddict" />
+        <button className="close-btn" onClick={toogleSideBar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((item) => {
+          const { id, url, text, icon } = item;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-icons">
+        {social.map((item) => {
+          const { id, url, icon } = item;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,31 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toogleSideBar = () => {
+    setIsSideBarOpen(!isSideBarOpen);
+  };
+  const toogleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{ isSideBarOpen, isModalOpen, toogleSideBar, toogleModal }}
+    >
+      {" "}
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
